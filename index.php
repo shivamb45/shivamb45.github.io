a676796912abb4602d8384cc5d180aadef26badb
Shivam Bharadwaj <shivamb45@yahoo.in>
2016-03-15 11:11:56 +0530

    testing for a php page on github

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="description" content="This website is official website of Shivam Bharadwaj. View his education, contact and other general details.">
+<meta name="keywords" content="Shivam Bharadwaj, Shivam, Bharadwaj, shivamb45, glbitm, shivam, bharadwaj, bhardwaj, shivam bhardwaj, sh1vam, Master Shivam, master shivam, bulandshahr, meerut, programmer, computer science student, cse student, shitx.xyz, shitx co-founder">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="custom-starter.css">
+ <title>Shivam Bharadwaj || Master Shivam || shivamb45</title>
+ <style>
+ body {
+  padding-top: 70px;
+}
+</style>
+  </head>
+
+ <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#"><div id="firstName">sh1vam</div>.in</a>
+        </div>
+		</nav>
+		 <div class="container theme-showcase" role="main">
+		<div class="jumbotron">
+        <h1>Hello! This is Official Website of Shivam Bharadwaj</h1>
+        <p class="lead">Choose any one version to enter the main website. Old or classic (this is the first version, best viewed on Desktop) or New responsive Website(recommended for mobiles).</p>
+        <?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+      </div>
+	 
+<p class="lead">
+<a href="http://classic.sh1vam.in/"><button type="button" class="btn btn-lg btn-primary">Classic</button></a> <a href="home.html"title="Link to Homepage of New Responsive Website"><button type="button" class="btn btn-lg btn-primary">New</button></a>
+</p>
+</div>
+ </body>
+</html>
